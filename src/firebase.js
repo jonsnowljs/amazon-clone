e9254c9331a2e3cf3bc77026169a4e68dc88db66
Jiasheng Lu <1473875731@qq.com>
2022-02-05 16:22:55 -0700

    init commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCvOJXICyVSWENkJxgJbUPwuVCBO6V-vOo',
+  authDomain: 'clone-de3f6.firebaseapp.com',
+  projectId: 'clone-de3f6',
+  storageBucket: 'clone-de3f6.appspot.com',
+  messagingSenderId: '921219780244',
+  appId: '1:921219780244:web:aec7afa289936ff7fc2e81',
+  measurementId: 'G-2T8FNHZJVW',
+};
