6651e892ddda10d28aa22d836239b83b4b2901e1
Jiasheng Lu <1473875731@qq.com>
2022-02-05 16:22:55 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello 🚀 </h1>
+      {/* Header */}
+      {/* Header */}
+    </div>
+  );
+}
+
+export default App;
