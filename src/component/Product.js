582e974e3577e538335452ab137c1b95bcf1d3c1
Jiasheng Lu <1473875731@qq.com>
2022-02-06 23:31:59 -0700

    create product component

--- src/component/Product.js
+++ src/component/Product.js
@@ -2,18 +2,22 @@ import React from 'react';
 import './Product.css';
 
 function Product() {
-  return <div className="product">
-    <div className="product__info">
-      <p>The lean startup</p>
-      <p className="product__price">
-        <small>$</small>
-        <strong>19.99</strong>
-      </p>
-      <div className='product__rating'>
-        <p>star</p>
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>star</p>
+        </div>
       </div>
+      <img src="https://m.media-amazon.com/images/I/71bhWgQK-cL._AC_SX679_.jpg" />
+      <button>Add to Basket</button>
     </div>
-  </div>;
+  );
 }
 
 export default Product;
